c5d5860db79a26c60d29119a77113387c1f3efe4
Robin Blondin <robin.blondin@protonmail.com>
2024-03-11 16:47:30 +0100

    Added eventlistener for add to cart button

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 import { processProducts, filterProductByCategory, filterProductBySearch, fetchProducts } from './fetchingLogic.js'
 
+const shoppingCart = new Map();
 const list = await fetchProducts();
 
 processProducts(list);
@@ -14,3 +15,26 @@ document.getElementById('search').addEventListener("search", function() {
     filterProductBySearch(value)
 
 })
+
+document.addEventListener('click', function(e) {
+    if (e.target && e.target.matches('button.add-to-cart-button')) {
+        console.log('hello')
+        const productId = e.target.getAttribute('button-id')
+        console.log(productId)
+        var cart = document.getElementById('cart-counts')
+        
+        cart.textContent = parseInt(cart.textContent) + 1
+
+        list.forEach(product => {
+            if(product.id == productId) {
+            if (shoppingCart.get(product) == null) {
+                shoppingCart.set(product, 1)
+            } else {
+                shoppingCart.set(product, shoppingCart.get(product) + 1)
+            }
+        }
+        })
+        cart.style.display = 'block'
+    }
+})
+
