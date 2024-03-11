2e5e739f821a7e8f187af15babdb475c80258d96
Robin Blondin <robin.blondin@protonmail.com>
2024-03-11 16:44:31 +0100

    Added product id to cart button

--- createCard.js
+++ createCard.js
@@ -1,17 +1,19 @@
-export function createCard(imageSrc, title, price) {
+export function createCard(id, imageSrc, title, price) {
     fetch('./card.html')
         .then(res => res.text())
         .then(data => {
             const temp = document.createElement('div')
-            temp.className = "card"
             temp.innerHTML = data
 
-            const card = temp.querySelector('.card')
+            const card = temp.firstElementChild;
 
+            card.querySelector('.add-to-cart-button').setAttribute('button-id', id)
             card.querySelector('.product-image').src = imageSrc
             card.querySelector('.product-title').textContent = title
             card.querySelector('.product-price').textContent = `$${price}`
             card.style.animationDelay = `${1 * 100}ms`;
+            
+            
 
             document.querySelector('.product-list').appendChild(card)
         })
