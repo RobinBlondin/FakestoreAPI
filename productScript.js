2e3798a55d73ef31652251cbaf9502959ce906df
Robin Blondin <robin.blondin@protonmail.com>
2024-03-15 11:32:00 +0100

    Added event for more-info button and close button

--- productScript.js
+++ productScript.js
@@ -9,7 +9,7 @@ const shoppingCart = JSON.parse(localStorage.getItem('cart') || '[]')
 console.log(shoppingCart.length)
 setCartCount(shoppingCart.length)
 
-document.getElementById('search').addEventListener("search", function () {
+document.getElementById('search').addEventListener("search", () => {
     const productPanel = document.getElementById('products')
     productPanel.innerHTML = '';
 
@@ -20,16 +20,27 @@ document.getElementById('search').addEventListener("search", function () {
 
 })
 
-document.addEventListener('click', function (e) {
-    if (e.target && e.target.matches('button.add-to-cart-button')) {
+document.addEventListener('click', e => {
+    if (e.target && (e.target.matches('button.add-to-cart-button') || e.target.matches('button.mi-addtocart'))) {
         const productId = e.target.getAttribute('button-id')
         incrementCartCount()
+        console.log(`id: ${productId}`)
 
         shoppingCart.push(productId)
         localStorage.setItem('cart', JSON.stringify(shoppingCart));
 
-    } else if (e.target && e.target.matches('img.cartButton')) {
-        console.log('hello')
+    } else if (e.target && e.target.matches('button.more-info-button')) {
+        const productId = e.target.getAttribute('button-id');
+        const product = list.find(product => product.id === parseInt(productId))
+        document.querySelector('.mi-addtocart').setAttribute('button-id', productId)
+
+        document.querySelector('.more-info-image').src = product.image
+        document.querySelector('.more-info-title').textContent = product.title
+        document.querySelector('.more-info-desc').textContent = product.description
+        document.querySelector('.more-info-price').textContent = `$${product.price}`
+        document.querySelector('.more-info-card').style.display = 'block'
+    } else if (e.target && e.target.matches('button.mi-close')) {
+        document.querySelector('.more-info-card').style.display = 'none'
     }
 })
 
